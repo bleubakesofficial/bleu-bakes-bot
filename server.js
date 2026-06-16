862141e5865ae7acf68d4aee85397585ebe424d1
bleubakesofficial <bleubakesofficial@gmail.com>
2026-06-16 14:19:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,32 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Bleu Bakes Bot Running");
+app.use(express.json());
+
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = "bleubakes123";
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  }
+
+  res.sendStatus(403);
 });
 
-const PORT = process.env.PORT || 3000;
+app.post("/webhook", (req, res) => {
+  console.log(JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.get("/", (req, res) => {
+  res.send("Bleu Bakes WhatsApp Bot Running");
+});
+
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
